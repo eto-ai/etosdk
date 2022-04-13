83b3138d9480308614b824ddc94017b7bfd01f4d
Lei Xu <eddyxu@gmail.com>
2022-04-13 11:11:29 -0700

    bump rikai version and add rikai deps

--- eto/spark.py
+++ eto/spark.py
@@ -1,8 +1,17 @@
 from pyspark.sql import SparkSession
 
+from rikai.spark.utils import get_default_jar_version
+
 
 def get_session():
+    rikai_jar_vers = get_default_jar_version(use_snapshot=False)
+
     spark = (
-        SparkSession.builder.appName("eto-sdk-spark").master("local[*]").getOrCreate()
+        SparkSession.builder.config(
+            "spark.jars.packages", f"ai.eto:rikai_2.12:{rikai_jar_vers}"
+        )
+        .appName("eto-sdk-spark")
+        .master("local[*]")
+        .getOrCreate()
     )
     return spark
